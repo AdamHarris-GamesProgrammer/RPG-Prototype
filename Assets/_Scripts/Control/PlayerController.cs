915449c7693e1e579812c9f3ea4ebeee8b5c37c0
Adam Harris <adam_harris@hotmail.com>
2020-06-29 15:53:55 +0100

    0x08: Basic Attack test

---
+++ Assets/_Scripts/Control/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+using RPG.Movement;
+using RPG.Combat;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Mover playerMover;
+
+        void Awake()
+        {
+            playerMover = GetComponent<Mover>();
+        }
+
+        void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            foreach(RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+
+            if (Input.GetAxis("Horizontal") != 0.0f || Input.GetAxis("Vertical") != 0.0f)
+            {
+                MoveWithKeyboard();
+            }
+        }
+
+        void MoveToCursor()
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(GetMouseRay(), out hit))
+            {
+                playerMover.MoveTo(hit.point);
+            }
+        }
+
+        void MoveWithKeyboard()
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            if (horizontalInput != 0.0f || verticalInput != 0.0f)
+            {
+                Vector3 newTarget = new Vector3(transform.position.x + horizontalInput, 0.0f, transform.position.z + verticalInput);
+                playerMover.MoveTo(newTarget);
+            }
+        }
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+}
--- Assets/_Scripts/Movement/Mover.cs
+++ Assets/_Scripts/Movement/Mover.cs
@@ -1,38 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-    private NavMeshAgent agent;
-    private Animator animator;
-
-    void Awake()
+    public class Mover : MonoBehaviour
     {
-        agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
+        private NavMeshAgent agent;
+        private Animator animator;
+
+        void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void MoveTo(Vector3 location)
+        {
+            agent.destination = location;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = agent.velocity;
+
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+            float speed = localVelocity.z;
+
+            animator.SetFloat("ForwardSpeed", speed);
+        }
+
     }
-
-    private void Update()
-    {
-        UpdateAnimator();
-    }
-
-    public void MoveTo(Vector3 location)
-    {
-        agent.destination = location;
-    }
-
-    private void UpdateAnimator()
-    {
-        Vector3 velocity = agent.velocity;
-
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-
-        float speed = localVelocity.z;
-
-        animator.SetFloat("ForwardSpeed", speed);
-    }
-
 }
+
