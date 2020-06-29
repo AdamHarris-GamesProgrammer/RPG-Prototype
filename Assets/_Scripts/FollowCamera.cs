5b746da7a3db93ddaf980d7ee589f37603a2ae4c
Adam Harris <adam_harris@hotmail.com>
2020-06-29 10:46:59 +0100

    0x02: Added click to move and basic camera follow script

---
+++ Assets/_Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    //Vector3 offset;
+    private void Start()
+    {
+        //offset = Camera.main.transform.position - target.position;
+    }
+
+    void Update()
+    {
+        transform.position = target.position;
+    }
+}
---
+++ Assets/_Scripts/Mover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    //The last ray that was fired
+    Ray lastRay;
+
+    private NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 25.0f, Color.green);
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.destination = hit.point;
+        }
+
+        lastRay = ray;
+    }
+
+}
