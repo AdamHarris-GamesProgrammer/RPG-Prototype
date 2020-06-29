2352b525e9441e072a4bd1638f02c9a05cbec260
Adam Harris <adam_harris@hotmail.com>
2020-06-29 14:33:54 +0100

    0x06: Implemented Keyboard controls as well

--- Assets/_Scripts/Movement/Mover.cs
+++ Assets/_Scripts/Movement/Mover.cs
@@ -9,10 +9,14 @@ public class Mover : MonoBehaviour
     Ray lastRay;
 
     private NavMeshAgent agent;
+    //private CharacterController controller;
+
+    [SerializeField] private float movementSpeed = 2.5f;
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        //controller = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -23,6 +27,20 @@ public class Mover : MonoBehaviour
             MoveToCursor();
         }
         Debug.DrawRay(lastRay.origin, lastRay.direction * 25.0f, Color.green);
+
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if(horizontalInput != 0.0f || verticalInput != 0.0f)
+        {
+            Vector3 newTarget = new Vector3(transform.position.x + horizontalInput, 0.0f, transform.position.z + verticalInput);
+            agent.destination = newTarget;
+        }
+
+        //controller.Move(new Vector3(horizontalInput, 0.0f, verticalInput) * movementSpeed * Time.deltaTime);
+
+        //agent.velocity = controller.velocity;
 
         UpdateAnimator();
     }
