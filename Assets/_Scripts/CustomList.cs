fd0b884a141da4cd7c632e64c4822eea1ac7ba4a
Adam Harris <adam_harris@hotmail.com>
2020-07-02 10:22:45 +0100

    0x20: Added custom forest editor framework

---
+++ Assets/_Scripts/CustomList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomList : MonoBehaviour
+{
+    [System.Serializable]
+    public class MyClass
+    {
+        public GameObject AnGO;
+        public int AnInt;
+        public float AnFloat;
+        public Vector3 AnVector3;
+        public int[] AnIntArray = new int[0];
+    }
+
+    public List<MyClass> MyList = new List<MyClass>(1);
+
+    void AddNew()
+    {
+        MyList.Add(new MyClass());
+    }
+
+    void Remove(int index)
+    {
+        MyList.RemoveAt(index);
+    }
+}
