aea32bc9a64f34659daf61f350fccff9f402f62a
Adam Harris <adam_harris@hotmail.com>
2020-07-07 09:37:19 +0100

    0x37: Added saving module

---
+++ Assets/_Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,40 @@
+﻿using RPG.Saving;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string saveFile = "save";
+
+        void Update()
+        {
+            //L Key loads the save file
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            //O Key saves the game
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            Debug.Log(Application.persistentDataPath);
+            GetComponent<SavingSystem>().Save(saveFile);
+        }
+
+        private void Load()
+        {
+            GetComponent<SavingSystem>().Load(saveFile);
+        }
+
+    }
+}
+
--- Assets/_Scripts/SceneManagement/SceneTarget.cs
+++ Assets/_Scripts/SceneManagement/SceneTarget.cs
@@ -28,25 +28,7 @@ namespace RPG.SceneManagement
             //Safety Check
             if (sceneName.Length != 0)
             {
-                //if (Debug.isDebugBuild)
-                //{
-                //    //Only check the scene name is valid in debug mode to save load times in the built game
-                //    if (ValidSceneName())
-                //    {
-                //        StartCoroutine("Transition");
-                //    }
-                //    else
-                //    {
-                //        Debug.LogError("[Scene Target]: " + gameObject.name + " scene target component has a invalid name set");
-                //    }
-                //}
-                //else
-                //{
-                //    StartCoroutine("Transition");
-                //}
-
                 StartCoroutine("Transition");
-
             }
             else
             {
@@ -55,63 +37,47 @@ namespace RPG.SceneManagement
             }
         }
 
-        //This method cycles through all the scene names in the game to check that the sceneName variable has a correct name
-        private bool ValidSceneName()
-        {
-            for (int i = 0; i <= SceneManager.sceneCount; i++)
+            private IEnumerator Transition()
             {
-                Scene validation = SceneManager.GetSceneByBuildIndex(i);
+                Fader fader = FindObjectOfType<Fader>();
 
-                if (validation.name == sceneName)
+                DontDestroyOnLoad(gameObject);
+
+                yield return StartCoroutine(fader.FadeIn());
+
+                yield return SceneManager.LoadSceneAsync(sceneName);
+
+                SceneTarget target = GetOtherSceneTarget();
+                UpdatePlayer(target);
+
+                yield return StartCoroutine(fader.FadeWait());
+
+                yield return StartCoroutine(fader.FadeOut());
+
+                Destroy(gameObject);
+            }
+
+            //This method moves the player to the spawnpoint of the new portal
+            private void UpdatePlayer(SceneTarget target)
+            {
+                GameObject player = GameObject.FindWithTag("Player");
+                player.transform.parent.position = target.spawnPoint.position;
+                player.GetComponent<NavMeshAgent>().Warp(target.spawnPoint.position);
+                player.transform.rotation = target.spawnPoint.rotation;
+            }
+
+            private SceneTarget GetOtherSceneTarget()
+            {
+                foreach (SceneTarget target in FindObjectsOfType<SceneTarget>())
                 {
-                    return true;
+                    if (target == this) continue;
+                    if (target.destination != destinationToGo) continue;
+
+                    return target;
                 }
+                return null;
             }
-            return false;
-        }
-
-        private IEnumerator Transition()
-        {
-            Fader fader = FindObjectOfType<Fader>();
-
-            DontDestroyOnLoad(gameObject);
-
-            yield return StartCoroutine(fader.FadeIn());
-            yield return SceneManager.LoadSceneAsync(sceneName);
-
-            SceneTarget target = GetOtherSceneTarget();
-            UpdatePlayer(target);
-
-            yield return StartCoroutine(fader.FadeWait());
-
-            yield return StartCoroutine(fader.FadeOut());
-
-
-            Destroy(gameObject);
-        }
-
-        private void UpdatePlayer(SceneTarget target)
-        {
-            GameObject player = GameObject.FindWithTag("Player");
-            player.GetComponent<NavMeshAgent>().Warp(target.spawnPoint.position);
-            //player.transform.position = target.spawnPoint.position;
-            player.transform.rotation = target.spawnPoint.rotation;
-
 
         }
-
-        private SceneTarget GetOtherSceneTarget()
-        {
-            foreach (SceneTarget target in FindObjectsOfType<SceneTarget>())
-            {
-                if (target == this) continue;
-                if (target.destination != destinationToGo) continue;
-
-                return target;
-            }
-            return null;
-        }
-
     }
-}
 
