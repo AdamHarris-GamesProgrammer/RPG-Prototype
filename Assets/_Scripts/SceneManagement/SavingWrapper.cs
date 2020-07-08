546eb19f56d646d500806c4f5b15344dcc58aeb9
Adam Harris <adam_harris@hotmail.com>
2020-07-08 10:18:01 +0100

    0x40: Option to load last scene loaded

--- Assets/_Scripts/SceneManagement/SavingWrapper.cs
+++ Assets/_Scripts/SceneManagement/SavingWrapper.cs
@@ -9,14 +9,18 @@ namespace RPG.SceneManagement
     public class SavingWrapper : MonoBehaviour
     {
         const string saveFile = "save";
-
+        [SerializeField] bool loadLastScene = false;
         IEnumerator Start()
         {
-            Fader fade = FindObjectOfType<Fader>();
-            fade.FadeOutImmediate();
-            
-            yield return GetComponent<SavingSystem>().LoadLastScene(saveFile);
-            yield return fade.FadeOut();
+            if (loadLastScene)
+            {
+                Fader fade = FindObjectOfType<Fader>();
+                fade.FadeOutImmediate();
+
+                yield return GetComponent<SavingSystem>().LoadLastScene(saveFile);
+                yield return fade.FadeOut();
+            }
+
         }
 
         void Update()
