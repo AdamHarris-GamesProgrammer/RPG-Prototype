15bf9da4f4bcb76163aaae585eebab07df2e0013
Adam Harris <adam_harris@hotmail.com>
2020-07-16 15:40:04 +0100

    0x64: Setup a dictionary to reduce amount of foreach loops used

--- Assets/_Scripts/Resources/Experience.cs
+++ Assets/_Scripts/Resources/Experience.cs
@@ -2,16 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Resources
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] float experiencePoints = 0;
+
 
         public void GainExperience(float xpIn)
         {
             experiencePoints += xpIn;
+        }
+
+        public float GetExperiencePoints()
+        {
+            return experiencePoints;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
         }
     }
 }
--- Assets/_Scripts/Stats/Progression.cs
+++ Assets/_Scripts/Stats/Progression.cs
@@ -1,31 +1,22 @@
 ﻿using UnityEngine;
-using System;
+using System.Collections.Generic;
 namespace RPG.Stats
 {
     [CreateAssetMenu(menuName = "RPG/Progression")]
     public class Progression : ScriptableObject
     {
         [SerializeField] ProgressionCharacterClass[] characterDevelopment;
-       
+
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
+
         public float GetStat(CharacterClass type, Stat stat, int level)
         {
-            float calculatedValue = 0;
+            BuildLookup();
 
-            foreach (ProgressionCharacterClass classType in characterDevelopment)
-            {
-                if (type != classType.characterType) continue;
+            float baseValue = lookupTable[type][stat][0];   //Element 0 is base value
+            float rate = lookupTable[type][stat][1];        //Element 1 is value increase per level
 
-                foreach(ProgressionStats currentStat in classType.stats)
-                {
-                    if (stat != currentStat.stat) continue;
-
-                    calculatedValue = currentStat.baseValue * Mathf.Pow((1 + currentStat.valueIncresePerLevel / 1), level);
-
-                    break;
-                }
-
-                break;
-            }
+            float calculatedValue = baseValue * Mathf.Pow((1 + rate / 1), level);
 
             if(calculatedValue == 0)
             {
@@ -33,6 +24,26 @@ namespace RPG.Stats
             }
 
             return calculatedValue;
+        }
+
+        private void BuildLookup()
+        {
+            if (lookupTable != null) return;
+
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
+            foreach(ProgressionCharacterClass classType in characterDevelopment)
+            {
+                var statLookUpTable = new Dictionary<Stat, float[]>();
+
+                foreach (ProgressionStats currentStat in classType.stats)
+                {
+                    statLookUpTable[currentStat.stat] = new float[] { currentStat.baseValue, currentStat.valueIncresePerLevel };
+                }
+
+                lookupTable[classType.characterType] = statLookUpTable;
+            }
+            
         }
 
         [System.Serializable]
@@ -47,8 +58,8 @@ namespace RPG.Stats
         class ProgressionStats
         {
             public Stat stat;
-            public float baseValue = 100f;
-            public float valueIncresePerLevel = 0.15f;
+            public float baseValue = 100f;              //Element 0 in statLookupTable
+            public float valueIncresePerLevel = 0.15f;  //Element 1 in statLookupTable
         }
     }
 }
