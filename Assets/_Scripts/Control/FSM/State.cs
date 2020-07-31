25a3369d34836713943ec3f199dbe5d0d9498130
Adam Harris <adam_harris@hotmail.com>
2020-07-31 11:32:42 +0100

    0x01: Implemented base state machine framework

---
+++ Assets/_Scripts/Control/FSM/State.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public abstract class State
+    {
+        protected Dictionary<Transition, StateID> stateMap = new Dictionary<Transition, StateID>();
+
+        protected StateID stateID;
+
+        public StateID ID { get { return stateID; } }
+
+        protected Vector3 destinationPos;
+        protected Vector3 waypoints;
+
+        public void AddTransition(Transition transition, StateID id)
+        {
+            //Check if the map already has this transition added
+            if (stateMap.ContainsKey(transition))
+            {
+                Debug.LogError("[Error: State.cs]: Transition is already inside stateMap");
+                return;
+            }
+
+            stateMap.Add(transition, id);
+        }
+
+        public void DeleteTransition(Transition transition)
+        {
+            if (stateMap.ContainsKey(transition))
+            {
+                stateMap.Remove(transition);
+                return;
+            }
+            Debug.LogError("[Error: State.cs]: Transition passed in was not in this states map");
+        }
+
+        public StateID GetOutputState(Transition transition)
+        {
+            if (stateMap.ContainsKey(transition))
+            {
+                return stateMap[transition];
+            }
+
+            Debug.LogError("[Error: StateMachine.cs]: " + transition + " Transition passed to the state was not found");
+            return StateID.None;
+        }
+
+        //This method checks if we should transition out of this state
+        public abstract void Reason(Transform player, Transform npc);
+
+        //This method acts on the state
+        public abstract void Act(Transform player, Transform npc);
+
+
+    }
+}
+
---
+++ Assets/_Scripts/Control/FSM/StateMachine.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public enum Transition
+    {
+        None = 0,
+        WaitTimeOver,
+        AtWaypoint,
+        PlayerInChaseDistance,
+        PlayerLeavesChaseDistance,
+        PlayerInAttackRange,
+        PlayerLeavesAttackRange
+    }
+
+    public enum StateID
+    {
+        None = 0,
+        Guard,
+        Patrol,
+        Chase,
+        Attack
+    }
+
+
+    public class StateMachine : MonoBehaviour
+    {
+        protected Transform playerTransform;
+
+        protected Vector3 destinationPos;
+
+        protected GameObject[] pointList;
+
+        private List<State> states;
+
+        private StateID currentStateID;
+        public StateID CurrentStateID { get { return currentStateID; } }
+
+        private State currentState;
+        public State CurrentState { get { return currentState; } }
+
+        public StateMachine()
+        {
+            states = new List<State>();
+        }
+
+        //Adds the passed in state
+        public void AddState(State state)
+        {
+            if (state == null) return;
+
+            if(states.Count == 0)
+            {
+                states.Add(state);
+                currentState = state;
+                currentStateID = state.ID;
+                return;
+            }
+
+            foreach(State loopState in states)
+            {
+                if(loopState.ID == state.ID)
+                {
+                    Debug.LogError("[Error: StateMachine.cs]: Trying to add a state that is already in states");
+                    return;
+                }
+            }
+
+            states.Add(state);
+        }
+
+        //Deletes the passed in state to the states list
+        public void DeleteState(StateID stateID)
+        {
+            if(stateID == StateID.None)
+            {
+                Debug.LogError("[Error: StateMachine.cs]: StateID is set to None");
+                return;
+            }
+
+            foreach (State state in states)
+            {
+                if (state.ID == stateID)
+                {
+                    states.Remove(state);
+                    return;
+                }
+            }
+
+            Debug.LogError("[Error: StateMachine.cs]: The state passed in was not on the list. Cannot delete state");
+        }
+
+        public void PerformTransition(Transition transition)
+        {
+            if(transition == Transition.None)
+            {
+                Debug.LogError("[Error: StateMachine.cs]: The passed in transition is set to none");
+                return;
+            }
+
+            StateID id = currentState.GetOutputState(transition);
+            if (id == StateID.None) return;
+
+            currentStateID = id;
+            foreach(State state in states)
+            {
+                if(state.ID == currentStateID)
+                {
+                    currentState = state;
+                    break;
+                }
+            }
+        }
+
+        protected virtual void Initialize() { }
+
+        protected virtual void StateUpdate() { }
+
+        protected virtual void StateFixedUpdate() { }
+
+        private void Start()
+        {
+            Initialize();
+        }
+
+        private void Update()
+        {
+            StateUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            StateFixedUpdate();
+        }
+    }
+}
+
