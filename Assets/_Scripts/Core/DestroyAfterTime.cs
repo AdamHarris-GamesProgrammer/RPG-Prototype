fedb4a6deede58ce1f1edac35c28c0a2c338935a
Adam Harris <adam_harris@hotmail.com>
2020-08-21 14:06:53 +0100

    0x129: Bird spawning system

---
+++ Assets/_Scripts/Core/DestroyAfterTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    [SerializeField] float lifetime = 15.0f;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
---
+++ Assets/_Scripts/Core/SpawnItemAfterTime.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItemAfterTime : MonoBehaviour
+{
+    [SerializeField] List<GameObject> itemList;
+
+    public List<Transform> spawnpoints;
+
+    [SerializeField] float timeBetweenSpawns = 15.0f;
+
+    float timer = 0.0f;
+
+    private void Start()
+    {
+        spawnpoints = new List<Transform>();
+
+        foreach(Transform trans in transform)
+        {
+            if (trans == transform) continue;
+
+            spawnpoints.Add(trans);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime;    
+
+        if(timer > timeBetweenSpawns)
+        {
+            timer = 0.0f;
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        GameObject objectToSpawn;
+
+        if(itemList.Count == 1)
+        {
+            objectToSpawn = itemList[0];
+        }
+        else
+        {
+            int index = Random.Range(0, itemList.Count);
+
+            objectToSpawn = itemList[index];
+        }
+
+
+        int spawnpointIndex = Random.Range(0, spawnpoints.Count);
+
+        //Spawn at a random spawnpoint index and assume its rotation
+        Instantiate(objectToSpawn, spawnpoints[spawnpointIndex].position, spawnpoints[spawnpointIndex].rotation, spawnpoints[spawnpointIndex]);
+    }
+
+}
