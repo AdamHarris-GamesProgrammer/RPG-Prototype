170f39ac15250f3f6dd22193db79336cf7b91b24
Adam Harris <adam_harris@hotmail.com>
2020-08-21 10:20:14 +0100

    0x127: Added a audio mod script that plays a random clip with random pitch variation

---
+++ Assets/_Scripts/Core/AudioModder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioModder : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> audioClips;
+    [SerializeField] float pitchMin = 0.1f;
+    [SerializeField] float pitchMax = 0.1f;
+
+    AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+
+    public void Play()
+    {
+        source.pitch = 1.0f;
+
+        int index = Random.Range(0, audioClips.Count);
+
+        source.clip = audioClips[index];
+
+        float pitchBeforeMod = source.pitch;
+
+        source.pitch = Random.Range(pitchBeforeMod - pitchMin, pitchBeforeMod + pitchMax);
+
+        Debug.Log("Pitch: " + source.pitch);
+
+        source.Play();
+    }
+}
