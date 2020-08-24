ab26bc28e59e156fae052d8fcee0713dc2efee21
Adam Harris <adam_harris@hotmail.com>
2020-08-24 10:46:34 +0100

    0x132: Added base inventory screen

---
+++ Assets/_Scripts/Item/Item.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "RPG/Create Item")]
+public class Item : ScriptableObject
+{
+    public string name = "Unnamed";
+    public Image icon = null;
+    public string description = "Description";
+    public int value = 23;
+}
