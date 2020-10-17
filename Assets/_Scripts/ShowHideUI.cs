f47b35b7c0ec21da3273755e02aefd115bf4d9b2
Adam Harris <adam_harris@hotmail.com>
2020-10-17 10:29:35 +0100

    Revert "0x134: implemented pausing time"
    This reverts commit d497e4dc632ebba3092eac9f5b400fe5c028b9a9.

--- Assets/_Scripts/ShowHideUI.cs
+++ Assets/_Scripts/ShowHideUI.cs
@@ -9,10 +9,6 @@ namespace InventoryExample.UI
         [SerializeField] KeyCode toggleKey = KeyCode.Escape;
         [SerializeField] GameObject uiContainer = null;
 
-        [Header("Gameplay Related Settings")]
-        [SerializeField] bool pauseTime = false;
-        [SerializeField] bool enablesMouse = false;
-
         // Start is called before the first frame update
         void Start()
         {
@@ -25,36 +21,6 @@ namespace InventoryExample.UI
             if (Input.GetKeyDown(toggleKey))
             {
                 uiContainer.SetActive(!uiContainer.activeSelf);
-
-                bool active = uiContainer.activeSelf;
-
-                if (pauseTime)
-                {
-                    if (active)
-                    {
-                        AudioListener.pause = true;
-                        Time.timeScale = 0.0f;
-                    }
-                    else
-                    {
-                        AudioListener.pause = false;
-                        Time.timeScale = 1.0f;
-                    }
-                }
-
-                if (enablesMouse)
-                {
-                    if (active)
-                    {
-                        Cursor.visible = true;
-                        Cursor.lockState = CursorLockMode.None;
-                    }
-                    else
-                    {
-                        Cursor.visible = false;
-                        Cursor.lockState = CursorLockMode.Locked;
-                    }
-                }
             }
         }
     }
